dbc47f5053cd452a54e5528b26d522f26e160d0e
zhangyao <45zhangye@gmail.com>
2023-11-14 02:03:17 +0800

    chore: delete cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/yaozhang/Desktop/brownant-fullstack-blog/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/yaozhang/Desktop/brownant-fullstack-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/yaozhang/Desktop/brownant-fullstack-blog/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/yaozhang/Desktop/brownant-fullstack-blog/src/css/custom.css"),
-];
